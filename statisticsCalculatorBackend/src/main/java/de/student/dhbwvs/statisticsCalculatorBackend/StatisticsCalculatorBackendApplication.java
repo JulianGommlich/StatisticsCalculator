b33412df0e26a43f3a9a83501c2cbcf29dc31159
JulianGommlich <julian.gommlich@gmx.de>
2021-01-15 17:01:14 +0100

    Initial Commit Backend

---
+++ statisticsCalculatorBackend/src/main/java/de/student/dhbwvs/statisticsCalculatorBackend/StatisticsCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package de.student.dhbwvs.statisticsCalculatorBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ statisticsCalculatorBackend/src/test/java/de/student/dhbwvs/statisticsCalculatorBackend/StatisticsCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.student.dhbwvs.statisticsCalculatorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
