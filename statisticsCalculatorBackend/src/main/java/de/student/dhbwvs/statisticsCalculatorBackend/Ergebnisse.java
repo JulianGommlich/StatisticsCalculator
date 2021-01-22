9001ae173d95079b83bae17db9979d039a1edf22
Davi2033 <63730971+Davi2033@users.noreply.github.com>
2021-01-22 13:37:26 +0100

    Update Ergebnisse.java

--- statisticsCalculatorBackend/src/main/java/de/student/dhbwvs/statisticsCalculatorBackend/Ergebnisse.java
+++ statisticsCalculatorBackend/src/main/java/de/student/dhbwvs/statisticsCalculatorBackend/Ergebnisse.java
@@ -1,7 +1,13 @@
 package de.student.dhbwvs.statisticsCalculatorBackend;
 
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Ergebnisse {
 
     private Map<Double, Integer> haeufigkeitsverteilung;
@@ -15,101 +21,5 @@ public class Ergebnisse {
     private double mittlereAbweichungZuZ;
     private double giniKoeffizient;
 
-
-    public Ergebnisse(Map<Double, Integer> haeufigkeitsverteilung, double[] expliziteStichprobe, double[] quantile,
-                      double modalwert, double mittelwert, double median, double varianz,
-                      double standardabweichung, double mittlereAbweichungZuZ, double giniKoeffizient) {
-
-        this.haeufigkeitsverteilung = haeufigkeitsverteilung;
-        this.expliziteStichprobe = expliziteStichprobe;
-        this.quantile = quantile;
-        this.modalwert = modalwert;
-        this.mittelwert = mittelwert;
-        this.median = median;
-        this.varianz = varianz;
-        this.standardabweichung = standardabweichung;
-        this.mittlereAbweichungZuZ = mittlereAbweichungZuZ;
-        this.giniKoeffizient = giniKoeffizient;
-    }
-
-    public Map<Double, Integer> getHaeufigkeitsverteilung() {
-        return haeufigkeitsverteilung;
-    }
-
-    public void setHaeufigkeitsverteilung(Map<Double, Integer> haeufigkeitsverteilung) {
-        this.haeufigkeitsverteilung = haeufigkeitsverteilung;
-    }
-
-    public double[] getExpliziteStichprobe() {
-        return expliziteStichprobe;
-    }
-
-    public void setExpliziteStichprobe(double[] expliziteStichprobe) {
-        this.expliziteStichprobe = expliziteStichprobe;
-    }
-
-    public double[] getQuantile() {
-        return quantile;
-    }
-
-    public void setQuantile(double[] quantile) {
-        this.quantile = quantile;
-    }
-
-    public double getModalwert() {
-        return modalwert;
-    }
-
-    public void setModalwert(double modalwert) {
-        this.modalwert = modalwert;
-    }
-
-    public double getMittelwert() {
-        return mittelwert;
-    }
-
-    public void setMittelwert(double mittelwert) {
-        this.mittelwert = mittelwert;
-    }
-
-    public double getMedian() {
-        return median;
-    }
-
-    public void setMedian(double median) {
-        this.median = median;
-    }
-
-    public double getVarianz() {
-        return varianz;
-    }
-
-    public void setVarianz(double varianz) {
-        this.varianz = varianz;
-    }
-
-    public double getStandardabweichung() {
-        return standardabweichung;
-    }
-
-    public void setStandardabweichung(double standardabweichung) {
-        this.standardabweichung = standardabweichung;
-    }
-
-    public double getMittlereAbweichungZuZ() {
-        return mittlereAbweichungZuZ;
-    }
-
-    public void setMittlereAbweichungZuZ(double mittlereAbweichungZuZ) {
-        this.mittlereAbweichungZuZ = mittlereAbweichungZuZ;
-    }
-
-    public double getGiniKoeffizient() {
-        return giniKoeffizient;
-    }
-
-    public void setGiniKoeffizient(double giniKoeffizient) {
-        this.giniKoeffizient = giniKoeffizient;
-    }
 
 }
