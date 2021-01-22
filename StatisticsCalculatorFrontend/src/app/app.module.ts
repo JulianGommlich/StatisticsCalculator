0a98a3b58ad73bafaf396e52338aa0ed93c6645f
Tobozz <Tobozz@users.noreply.github.com>
2021-01-22 14:52:55 +0100

    view2 Komponenten hinzugefügt und etwas bearbeitet

--- StatisticsCalculatorFrontend/src/app/app.module.ts
+++ StatisticsCalculatorFrontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { View1Component } from './view1/view1.component';
+import { View2AnzeigeStichprobenComponent } from './view2-anzeige-stichproben/view2-anzeige-stichproben.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    View1Component
+    View1Component,
+    View2AnzeigeStichprobenComponent
   ],
   imports: [
     BrowserModule,
