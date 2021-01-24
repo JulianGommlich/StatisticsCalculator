25ac7d9f9dd268d7f0562344c90d701126877b95
Tobozz <Tobozz@users.noreply.github.com>
2021-01-24 18:09:41 +0100

    R.I.P meine Freizeit (niklas' btw)

--- StatisticsCalculatorFrontend/src/app/view1/view1.component.ts
+++ StatisticsCalculatorFrontend/src/app/view1/view1.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PopUpInvalidComponent } from '../pop-up-invalid/pop-up-invalid.component';
+import { PopUpComponent } from '../pop-up/pop-up.component';
 
 @Component({
   selector: 'app-view1',
   templateUrl: './view1.component.html',
   styleUrls: ['./view1.component.css']
 })
-export class View1Component implements OnInit {
 
-  constructor() { }
+export class View1Component {
+  constructor(public dialog: MatDialog) {
 
-  ngOnInit(): void {
+  }
+
+  openDialog() {
+    console.log("test");
+    const dialogRef = this.dialog.open(PopUpComponent, {
+      data: { fix: false, absolute: [1,2,3,4] }  //Auffang für Daten aus dem Backend
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
