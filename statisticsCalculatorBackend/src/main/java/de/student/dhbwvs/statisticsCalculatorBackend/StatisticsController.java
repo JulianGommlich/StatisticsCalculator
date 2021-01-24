cdf3be7ee93e41ef343444cf523d5ec8de9caa63
Davi2033 <david.arnold.plus@gmail.com>
2021-01-24 19:27:41 +0100

    API-Controller

--- statisticsCalculatorBackend/src/main/java/de/student/dhbwvs/statisticsCalculatorBackend/StatisticsController.java
+++ statisticsCalculatorBackend/src/main/java/de/student/dhbwvs/statisticsCalculatorBackend/StatisticsController.java
@@ -2,24 +2,23 @@ package de.student.dhbwvs.statisticsCalculatorBackend;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping (path = "/exchange")
 public class StatisticsController {
 
-    Map<Double, Integer> map = new LinkedHashMap<>();
-
     private static Ergebnisse ergebnisse = new Ergebnisse();
 
+    //ich verstehs nicht
     @GetMapping
-    public Ergebnisse getErgebnisse() {
-        return ergebnisse;
+    public String getErgebnisse() {
+        return "Hallo";
     }
 
+    //empfängt einen String und gibt ihn zurück
     @PostMapping
-    public Ergebnisse createCustomer(@RequestBody Ergebnisse ergebnisse) {
+    public Ergebnisse neuesErgebnis(@RequestBody int mittelwert) {
+        ergebnisse.setMedian(6.7635);
+        ergebnisse.setMittelwert(mittelwert);
         return ergebnisse;
     }
 }
