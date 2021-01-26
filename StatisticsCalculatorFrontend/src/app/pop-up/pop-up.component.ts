1e7306c3cfe789a231e740cf46b8d59f85677814
Joemollytana <andrey-shkolnik@protonmail.com>
2021-01-26 16:30:45 +0100

    made function getResults() in popup for getting the http things

--- StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
+++ StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pop-up',
@@ -12,14 +13,18 @@ export class PopUpComponent implements OnInit {
   absolute: number[] = [];
   explicite: number[] = [];
   
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {fix: boolean, absolute: number[], explicite?: number[]}) { 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {fix: boolean, absolute: number[], explicite?: number[]}, private router:Router) { 
     this.fix=data.fix; 
     this.absolute=data.absolute; 
     if(data.explicite){
       this.explicite=data.explicite;
     }  
   }
-
+  getResults(){
+    // Create logic to get data
+    this.router.navigate(['/results']);;
+  }
+  
   ngOnInit(): void {
   }
 
