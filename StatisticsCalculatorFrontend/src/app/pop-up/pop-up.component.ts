7e801687fca78396b34a5b47efab32f90ca0d32a
davidbhrs <d.baehrens@web.de>
2021-01-28 21:59:52 +0100

    add Umrechungslogik

--- StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
+++ StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { ApiEndpointService } from '../api-endpoint.service';
-import { Stichprobe } from '../stichprobe';
+import { SampleType, Stichprobe } from '../stichprobe';
 
 @Component({
   selector: 'app-pop-up',
@@ -16,6 +16,10 @@ export class PopUpComponent implements OnInit {
   explicite: number[] = [];
   // Response vom Backend
   result: any;
+
+  // Test Input
+  inputData = new Stichprobe(SampleType.explicit, [1, 5, 1, 8, 1, 2, 9, 4, 6, 5, 6, 5, 4], { 2: 1, 5: 3, 4: 2, 1: 3, 8: 1, 9:1, 6: 2 }, 3);
+
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { fix: boolean, absolute: number[], explicite?: number[] }, private router: Router, public apiEndpoint: ApiEndpointService) {
     this.fix = data.fix;
@@ -35,7 +39,12 @@ export class PopUpComponent implements OnInit {
   // send sample to API-Endpoint-Service
   startCalculation(): void {
     // Für STICHPROBENOBJEKT müssen die Daten aus der View eingefügt werden (Issue 16) --> Auskommentiert um Fehler zu vermeiden. 
-    //this.apiEndpoint.startCalculation(this.STICHPROBENOBJEKT).subscribe(sample => this.result = sample);
+    //this.apiEndpoint.startCalculation(this.inputData).subscribe(sample => this.result = sample);
+    
+    
+    // Auskommentiert fürs testen!!!!
+    this.inputData.setExpSample();
+    this.inputData.setFreqDistribution();
   }
 
 }
--- StatisticsCalculatorFrontend/src/app/stichprobe.ts
+++ StatisticsCalculatorFrontend/src/app/stichprobe.ts
@@ -1,13 +1,72 @@
+import { ValueConverter } from "@angular/compiler/src/render3/view/template";
+
+
+
+export enum SampleType {
+  explicit = "explicit",
+  absolute = "absolute"
+}
+
+
+
 export class Stichprobe {
 
-  sampleType: string;
-  values: object[];
+  sampleType: SampleType;
+  explSample: number[];
+  //freqDist: object;
+  freqDist: { [key: string]: number };
   z: number;
 
-  constructor(newSampleType: string, newValues: object[], newZ: number) {
+
+
+  constructor(newSampleType: SampleType, newExplSample: number[], newFreqDist: { [key: string]: number }, newZ: number) {
     this.sampleType = newSampleType;
-    this.values = newValues;
+    this.explSample = newExplSample;
+    this.freqDist = newFreqDist;
     this.z = newZ;
+  }
+
+
+  // converts the explicit sample to frequency distribution
+  setFreqDistribution(): void {
+    let newFreqDist: { [key: string]: number } = {};
+
+    // für jeden Wert im Array wird ein neuer key angelegt, falls noch nicht vorhanden, oder der value(die Anzahl) um eins erhöht:
+    this.explSample.forEach(function (key) {
+      if (Object.keys(newFreqDist).includes(String(key))) {
+        newFreqDist[key] += 1;
+      }
+      else {
+        Object.assign(newFreqDist, { [key]: 1 });
+      }
+    });
+
+    // set the property "freqDist" to the new calulated frequency distribution
+    this.freqDist = newFreqDist;
+
+    console.log(this.freqDist);
+  }
+
+
+  // converts the frequency distribution to explicit sample
+  setExpSample(): void {
+    let newExplSample: number[] = [];
+    let sortedExplSample: number[] = [];
+
+    // für jeden key in der Häufigkeitsverteilung (Object) wird jeweils die Häufigkeit (values) an Werten (keys) in das Array gepusht. 
+    for (let key in this.freqDist) {
+      for (let i = this.freqDist[key]; i > 0; i--) {
+        newExplSample.push(Number(key));
+      }
+    }
+
+    // aufsteigende Sortierung des Arrays
+    sortedExplSample = newExplSample.sort((n1, n2) => n1 - n2);
+
+    // set the property "explSample" to the new calulated explicit Sample
+    this.explSample = sortedExplSample;
+
+    console.log(this.explSample);
   }
 
 }
