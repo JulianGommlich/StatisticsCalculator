d712dd0a0eb72f570697bff3eb739138afb283d1
Joemollytana <andrey-shkolnik@protonmail.com>
2021-01-29 18:42:54 +0100

    schwierigkeiten beim validieren

--- StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
+++ StatisticsCalculatorFrontend/src/app/pop-up/pop-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-pop-up',
@@ -9,12 +10,13 @@ import { Router } from '@angular/router';
 })
 export class PopUpComponent implements OnInit {
 
-  fix = false;
+  fix: BehaviorSubject<boolean>;
   absolute: number[] = [];
   explicite: number[] = [];
   
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {fix: boolean, absolute: number[], explicite?: number[]}, private router:Router) { 
-    this.fix=data.fix; 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {fix: boolean, absolute: number[], explicite?: number[]},
+   private router:Router) { 
+    this.fix =new BehaviorSubject(data.fix); 
     this.absolute=data.absolute; 
     if(data.explicite){
       this.explicite=data.explicite;
--- StatisticsCalculatorFrontend/src/app/view1/view1.component.ts
+++ StatisticsCalculatorFrontend/src/app/view1/view1.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiEndpointService } from '../api-endpoint.service';
-import { Stichprobe } from '../stichprobe';
 import { MatDialog } from '@angular/material/dialog';
 import { PopUpInvalidComponent } from '../pop-up-invalid/pop-up-invalid.component';
 import { PopUpComponent } from '../pop-up/pop-up.component';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-view1',
@@ -15,6 +15,7 @@ import { Router } from '@angular/router';
 export class View1Component {
   // Response vom Backend
   result: any;
+
   constructor(public dialog: MatDialog, private router: Router, public apiEndpoint: ApiEndpointService) {
   }
 
@@ -23,19 +24,61 @@ export class View1Component {
   }
 
   openDialog() {
-    console.log("test");
-    const dialogRef = this.dialog.open(PopUpComponent, {
-      data: { fix: true, absolute: [1,2,3,4] }  //Auffang für Daten aus dem Backend
-    });
-  
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
+    //console.log("test");
+    var fixData = null;
+    var validationTrue = this.checkValidation();
+    const expl = document.getElementById('explSample') as HTMLInputElement;
+    const abs = document.getElementById('absSample') as HTMLInputElement;
+    if (validationTrue == true){
+      if (expl.checked == true && abs.checked == false){
+        fixData = true;
+      } else if (abs.checked == true && expl.checked == false){
+        fixData = false;
+      } 
+      const dialogRef = this.dialog.open(PopUpComponent, {
+      data: { fix: fixData, absolute: [1,2,3,4] }})  //Auffang für Daten aus dem Backend
+    
+    } else {
+      const dialogRef = this.dialog.open(PopUpInvalidComponent, {
+        data: {}}) //Auffang für Daten aus dem Backend}
+      }            
   }
-
-  // send sample to API-Endpoint-Service
-  startCalculation(): void {
-    // Für STICHPROBENOBJEKT müssen die Daten aus der View eingefügt werden (Issue 16) 
-    this.apiEndpoint.startCalculation(this.STICHPROBENOBJEKT).subscribe(sample => this.result = sample);
+  checkValidation(){
+    var validation;
+    const expl = document.getElementById('explSample') as HTMLInputElement;
+    const abs = document.getElementById('absSample') as HTMLInputElement;
+    const valueZ = document.getElementById('valueZInput') as HTMLInputElement;
+    const deviation = document.getElementById('meanDeviation') as HTMLInputElement;
+    if (expl.checked == false && abs.checked == false){
+      return validation = false;
+    } else if(valueZ.value.length == 0){
+      return validation = false;      
+    } else if(deviation.value.length == 0){
+      return validation = false; 
+    } else if(this.validateSequence() == false){
+      return validation = false;  
+    } else {
+      return validation = true;
+    }
+  }
+  validateSequence(){
+    let numSeq : string = (<HTMLInputElement>document.getElementById("numSequence")).value;   
+    var letters = /[A-Za-z]$/;
+    
+    if (numSeq.length == 0){
+      return false
+    } else if (numSeq.match(letters)){
+      return false
+    }
+    let splittedSeq: string[] = numSeq.split(";", 100)
+    alert (splittedSeq);
   }
 }
+  
+
+  
+    // send sample to API-Endpoint-Service
+    //startCalculation(): void {
+    // Für STICHPROBENOBJEKT müssen die Daten aus der View eingefügt werden (Issue 16) 
+    //this.apiEndpoint.startCalculation(this.STICHPROBENOBJEKT).subscribe(sample => this.result = sample);
+    //}
