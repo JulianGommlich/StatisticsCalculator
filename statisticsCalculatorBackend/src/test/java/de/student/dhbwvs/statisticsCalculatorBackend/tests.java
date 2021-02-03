c1fb1ab477d88513b51c2a6cfa52d75f46d5eea6
Philipp <60327768+PhilippBaur@users.noreply.github.com>
2021-02-03 18:53:41 +0100

    Create tests.java

---
+++ statisticsCalculatorBackend/src/test/java/de/student/dhbwvs/statisticsCalculatorBackend/tests.java
@@ -0,0 +1,9 @@
+package de.student.dhbwvs.statisticsCalculatorBackend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static de.student.dhbwvs.statisticsCalculatorBackend.calculations.*;
